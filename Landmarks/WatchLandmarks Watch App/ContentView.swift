eb95e7c5ea01e8ecb0b2093ab5cf88d2e7e5b608
Wole Solana <4244148+wolesolana@users.noreply.github.com>
2024-03-08 10:26:47 -0600

    Add watchOS target to project

---
+++ Landmarks/WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Wole Solana on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
